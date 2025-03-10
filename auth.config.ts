eb5a5aab9ea545c8fb469154c4e0ade4ca2dd571
fede-longhi <federico.rlonghi@gmail.com>
2025-03-09 23:18:59 -0300

    remove dashboard pages

--- auth.config.ts
+++ auth.config.ts
@@ -10,12 +10,12 @@ export const authConfig = {
         authorized({ auth, request: { nextUrl } }) {
             console.log('authorized');
             const isLoggedIn = !!auth?.user;
-            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            const isOnDashboard = nextUrl.pathname.startsWith('/home');
             if (isOnDashboard) {
                 if (isLoggedIn) return true;
                 return false; // Redirect unauthenticated users to login page
             } else if (isLoggedIn) {
-                return Response.redirect(new URL('/dashboard', nextUrl));
+                return Response.redirect(new URL('/home', nextUrl));
             }
             return true;
         },
